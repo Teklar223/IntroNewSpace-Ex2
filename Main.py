fedfef5b1c49babdca7cd652e93b674eac396750
Yonatan Ratner <yonaratner@gmail.com>
2023-05-20 15:45:00 +0300

    migrated to pygame

--- Main.py
+++ Main.py
@@ -1,13 +1,60 @@
-import Simulation
+from Simulation import Simulation
+import pygame
+from pygame.locals import *
+from random import randint
+from Configuration import Configuration
+from Spaceship import Spaceship
+
+# Constants
+UP = K_w
+DOWN = K_s
+LEFT = K_a
+RIGHT = K_d
+
+class SpaceGame:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800, 600))
+        self.clock = pygame.time.Clock()
+        self.ship = None
+
+    def _handle_events(self):
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return False
+            elif event.type == KEYDOWN:
+                if event.key == UP:
+                    self.ship.up_fun()
+                elif event.key == DOWN:
+                    self.ship.down_fun()
+                elif event.key == LEFT:
+                    self.ship.left_fun()
+                elif event.key == RIGHT:
+                    self.ship.right_fun()
+
+        return True
+
+    def start(self):
+        config = Configuration()  # kwargs = None --> default config.
+        self.ship = Spaceship(config)
+        running = True
+
+        while running:
+            dt = self.clock.tick(60) / 1000.0
+            running = self._handle_events()
+
+            self.ship.update(dt, self.screen.get_width(), self.screen.get_height())
+
+            self.screen.fill((0, 0, 0))
+            self.screen.blit(self.ship.image, self.ship.rect)
+            pygame.display.flip()
+
+        pygame.quit()
 
 def main():
-    sim = Simulation()
-    #TODO: implement...
-    '''
-        this class represents our "MAIN MENU", this should manage things such as:
-        1. loading/saving configs (ship starting state)
-        2. start the simulation
-    '''
+    game = SpaceGame()
+    game.start()
 
 if __name__ == "__main__":
     main()
--- Spaceship.py
+++ Spaceship.py
@@ -1,17 +1,68 @@
 import Configuration
+import pygame
 
 class Spaceship():
     '''
     '''
     def __init__(self, config : Configuration):
         # TODO: get some of these parameters as input (the starting point) and set them via config
-        self.vs = config.vs
-        self.hs = config.hs
-        self.dist = config.dist
-        self.ang =config.ang  # zero is vertical (as in landing)
-        self.alt = config.alt  # 2:25:40 (as in the simulation) # https://www.youtube.com/watch?v=JJ0VfRL9AMs
-        self.acc = config.acc  # Acceleration rate (m/s^2)
-        self.fuel = config.fuel
-        self.weight = config.WEIGHT_EMP + self.fuel
-        self.NN = config.NN  # engine power rate (in the range [0,1]), higher = more braking power
+        self.vs     =   config.vs
+        self.hs     =   config.hs
+        self.dist   =   config.dist
+        self.ang    =   config.ang  # zero is vertical (as in landing)
+        self.alt    =   config.alt  # 2:25:40 (as in the simulation) # https://www.youtube.com/watch?v=JJ0VfRL9AMs
+        self.acc    =   config.acc  # Acceleration rate (m/s^2)
+        self.fuel   =   config.fuel
+        self.weight =   config.WEIGHT_EMP + self.fuel
+        self.NN     =   config.NN  # engine power rate (in the range [0,1]), higher = more braking power
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self, config, *groups):
+        # *** Pygame ***
+        super().__init__(*groups)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill((255, 255, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (0, 0)
+
+        # *** Physics ***
+                
+        self.vs     =   config.vs
+        self.hs     =   config.hs
+        self.dist   =   config.dist
+        self.ang    =   config.ang  # zero is vertical (as in landing)
+        self.alt    =   config.alt  # 2:25:40 (as in the simulation) # https://www.youtube.com/watch?v=JJ0VfRL9AMs
+        self.acc    =   config.acc  # Acceleration rate (m/s^2)
+        self.fuel   =   config.fuel
+        self.weight =   config.WEIGHT_EMP + self.fuel
+        self.NN     =   config.NN  # engine power rate (in the range [0,1]), higher = more braking power
+        self.velocity = [config.hs, config.vs]
+        self.angle = config.ang
+
+        # *** Control ***
+        self.is_player = False
+
+    def up_fun(self):
+        print("UP")
+        self.config.NN = min(1.0, self.config.NN + 0.1)
+
+    def down_fun(self):
+        print("DOWN")
+        self.config.NN = max(0.0, self.config.NN - 0.1)
+    
+    def left_fun(self):
+        print("LEFT")
+        self.config.ang = (self.config.ang + 3) % 360
+
+    def right_fun(self):
+        print("RIGHT")
+        self.config.ang = (self.config.ang - 3) % 360
+
+    def update(self, dt, width, height):
+        pass
+
+    def set_player(self):
+         self.is_player = True
+    
+    def set_simulation(self):
+         self.is_player = False
 
