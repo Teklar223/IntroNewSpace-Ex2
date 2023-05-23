72fac7bc250bc848ea710b3b4653fec23a86ecb3
Yonatan Ratner <yonaratner@gmail.com>
2023-05-23 15:36:49 +0300

    basic editable input for config (doesnt update yet)

--- SpaceGame.py
+++ SpaceGame.py
@@ -4,7 +4,7 @@ from random import randint
 from Configuration import Configuration
 from Spaceship import Spaceship
 from Engine import Engine
-from Util import topg # to pygame (co-ordinates)
+from Util import InputBox,topg # to pygame (co-ordinates)
 from Constants import *
 
 # wasd Constants
@@ -37,13 +37,20 @@ class SpaceGame:
         self.startMenu()
     
     def startMenu(self):
-        '''
-            TODO:
-            1. configuration (editable)
-            2. start 1-player game button (which starts game with config.is_player = True)
-            3. start simulation button (we can try to simuulate bereshits landing statically, or using a method/similar)
-        '''
-        config = Configuration()  # kwargs = None <--> default config.
+        config = Configuration()  # Create a new configuration object
+        input_boxes = []  # List to store the input boxes
+
+        # Create input boxes for each configuration variable
+        y_offset = 10
+        for key, value in config.__dict__.items():
+            # TODO: render only the values we care about
+            # TODO: update the config after a value is set
+            permatxt = f"{key}: "
+            x = self.screen.get_width() - 210
+            input_box = InputBox(x, y_offset, 200, 30, text=str(value), permatext=permatxt)
+            input_boxes.append(input_box)
+            y_offset += 40
+
         start_button_rect = pygame.Rect(300, 200, 200, 100)  # Rect for the start button
         running = True
 
@@ -52,11 +59,11 @@ class SpaceGame:
                 if event.type == QUIT:
                     pygame.quit()
                     return
-                if event.type == MOUSEBUTTONDOWN and event.button == 1:
-                    if start_button_rect.collidepoint(event.pos):
-                        self.startGame(config)
 
-            # Clear the screen
+                # Handle events for the input boxes
+                for input_box in input_boxes:
+                    input_box.handle_event(event)
+
             self.screen.fill((255, 255, 255))
 
             # Draw the start button
@@ -66,13 +73,23 @@ class SpaceGame:
             self.screen.blit(start_button_text, start_button_text_rect)
 
             # Render and blit configuration values
-            self.render_config_values(config)
+            for input_box in input_boxes:
+                input_box.update()
+                input_box.draw(self.screen)
 
             pygame.display.flip()
 
+            # Check if the start button is clicked
+            if pygame.mouse.get_pressed()[0] and start_button_rect.collidepoint(pygame.mouse.get_pos()):
+                # Update the configuration variables
+                for input_box in input_boxes:
+                    setattr(config, input_box.permatext, input_box.text)
+                self.startGame(config)
+                running = False
+
+
     def render_config_values(self, config):
         # TODO: render only the values we care about
-        # TODO: make editable
         y_offset = 10
         for key, value in config.__dict__.items():
             text = f"{key}: {value}"
@@ -89,6 +106,9 @@ class SpaceGame:
             self.screen.blit(text_surface, text_surface.get_rect())
 
     def startGame(self, config):
+        # Clear the screen
+        self.screen.fill((255, 255, 255))
+
         x,y = topg(self.screen.get_width()/2, 0.9 * self.screen.get_height(), self.screen.get_height())
         self.ship = Spaceship(config,init_x = x, init_y = y)
         self.engine = Engine(config)
